a82c1da1fffbb453b82004111ae81696e5661242
Prananda <prananda1996@gmail.com>
2024-01-27 12:11:43 +0700

    27 Januari 2023

--- task1.js
+++ task1.js
@@ -6,11 +6,19 @@
  */
 function countOccurrences(numbers, target) {
   // write your code here
+  let match = []
+  for (i = 0 ; i < numbers.length ; i++){
+    if(numbers[i] === target){
+      match.push(numbers[i])
+    }
+  }
+  const angkasama = match.length
+  return angkasama
 }
 
 console.log(countOccurrences([1,2,3,4,2], 2)) // 2
-// console.log(countOccurrences([4,7,2,4,4,2], 4)) // 3
-// console.log(countOccurrences([2,1,1,2], 3)) // 0
+console.log(countOccurrences([4,7,2,4,4,2], 4)) // 3
+console.log(countOccurrences([2,1,1,2], 3)) // 0
 
 module.exports = {
   countOccurrences
--- task2.js
+++ task2.js
@@ -6,10 +6,17 @@
  */
 function filterWordsByLength(words, minLength) {
   // write your code here
+   let match = []
+  for (i = 0 ; i < words.length ; i++){
+    if (words[i].length >= minLength){
+      match.push(words[i])
+    }
+  }
+  return match
 }
 
 console.log(filterWordsByLength(['orange', 'pear', 'fig'], 4)) // ['orange', 'pear']
-// console.log(filterWordsByLength([['carrot', 'potato', 'broccoli'], 7)) // ['broccoli']
+console.log(filterWordsByLength(['carrot', 'potato', 'broccoli'], 7)) // ['broccoli']
 
 module.exports = {
   filterWordsByLength
